a8d37c12253420df93fe562ddc46afdb13ec656a
Daniela <dani.velasquezv@gmail.com>
2020-11-27 12:04:47 -0300

    select

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
+    <b-form-select >
+      <b-form-select-option v-model="selected" :value="null">Selecciona una raza</b-form-select-option>
+      <b-form-select-option v-for="(raza,i) in razas" :key="i" :value="null">{{raza.data.nombre}}</b-form-select-option>
+    </b-form-select>
+    
+    <!--
     <tr v-for="(raza,i) in razas" :key="i">
           <h2 class="text-center">{{raza.data.nombre}}</h2>
-    </tr>
+    </tr--> 
   </div>
 </template>
 
