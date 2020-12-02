eb9851cedd5eed2742b8a86f02821d06489d324e
Daniela <dani.velasquezv@gmail.com>
2020-12-02 12:35:14 -0300

    Se elimnan cambios de filtro

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,14 +18,6 @@
     <div class="mt-3">Selected2: <strong>{{ selected2 }}</strong></div>
   </div>
   <b-button class="mb-3" @click="goMuestra" variant="success">Buscar</b-button>
-<!-- temporal -->
-    <b-form-select class="my-5" v-model="selected1">
-      <b-form-select-option class="select" :value="null" >Selecciona una raza</b-form-select-option>
-      <b-form-select-option class="select" v-for="(persona,i) in personas" :key="i" :value="persona.data.city">{{persona.data.city}}</b-form-select-option>
-    </b-form-select>
-    <b-btn @click="getFiltradas('M')">BOTON2</b-btn>
-    <!-- -->
-
 </b-container>
 </div>
 </template>
@@ -35,14 +27,6 @@ import firebase from "firebase"
 import {mapState, mapGetters, mapActions} from "vuex"
 import router from "../router/index.js"
   export default {
-    state: {
-    personas : []
-  },
-    mutations: {
-      GET_DATA_PERSONAS1(state, personas){
-        state.personas = personas
-      }
-  },
     data() {
       return {
         selected1 : null,
@@ -60,38 +44,16 @@ import router from "../router/index.js"
             rango: "",
             codigo: ""
           }
-        },
-        persona: {
-          data: {
-            name: "",
-            sex: ""
-          }
         }
       }  
     },
     methods: {
-      ...mapActions(['getDataPersonasFiltradas', 'getDataFiltradas']),
        goMuestra() {
          this.$router.replace("/muestra")
        },
-       getFiltradas({commit}, sexo) {
-         console.log("valor de sexo:" + sexo)
-          firebase
-          .firestore()
-          .collection("personas")
-          .where("sex","==",sexo)
-          .onSnapshot((snapshot) => {
-            let personas = [];
-            snapshot.forEach((p) => {
-              personas.push({ id: p.id, data: p.data() });
-            });
-          commit("GET_DATA_PERSONAS1", personas);
-          });
-          
-       }
     },
     computed: {
-    ...mapState(['razas','edades','personas']),
+    ...mapState(['razas','edades']),
     }
   }
   
