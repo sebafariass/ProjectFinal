fe38ae7e167bf12dc13e2830e8a8f17b600ed495
Daniela <dani.velasquezv@gmail.com>
2020-12-01 22:25:24 -0300

    Cambios Login y Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,9 @@
 
-
-
 <template>
+<div class="home__pagina">
 <b-container class="bv-example-row">
   <div>
-     <h2>Hola! (nombre del perro)</h2>
-    <h5>Mostrar fotos del perro que están en la base de datos subidas por los dueños en el login</h5>
+     <h2>Bienvenid@</h2>
     <h2>¿Qué buscas?</h2>
     <b-form-select class="my-5">
       <b-form-select-option  class="select" v-model="selected" :value="null">Selecciona una raza</b-form-select-option>
@@ -17,11 +15,14 @@
       <b-form-select-option v-for="(edad,j) in edades" :key="j" :value="null">{{edad.data.rango}}</b-form-select-option>
     </b-form-select>
   </div>
+  <b-button class="mb-3" @click="goMuestra" variant="success">Buscar</b-button>
 </b-container>
+</div>
 </template>
 
 <script>
-import {mapState, mapGetters, mapActions} from "vuex";
+import {mapState, mapGetters, mapActions} from "vuex"
+import router from "../router/index.js"
   export default {
     data() {
       selected : "null"
@@ -40,13 +41,30 @@ import {mapState, mapGetters, mapActions} from "vuex";
         }
       }  
     },
-      computed: {
+    methods: {
+       goMuestra() {
+         this.$router.replace("/muestra")
+       } 
+    },
+    computed: {
     ...mapState(['razas','edades']),
-  },
+    }
   }
   
 </script>
 
 <style lang="scss">
+.home__pagina {
+  background: url("../assets/bg-home.jpg") no-repeat center fixed;
+  height: 100%;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+h2 {
+  font-family:"Sansita Swashed", cursive;
+}
+
 
 </style>
