42130f104d028d66526ffccf0c3df17417e54812
Sebastian Farias <sfarias.arellano@gmail.com>
2020-12-03 16:50:10 -0300

    testingFinalizado

--- tests/e2e/specs/Crearuser.js
+++ tests/e2e/specs/Crearuser.js
@@ -1,19 +1,1 @@
 
-
-describe('test crear usuario', () => {
-	it('Test de crear usuario', () => {
-       
-      
-	
-		cy.visit('/iniciosesion')
-		cy.get('#nombre_usuario').type('Giorgioaaa')
-		cy.get('#email_usuario').type('holaaaaaaddoooooooosssssooaaaa@gmail.com')
-		cy.get('#address_usuario').type('tegualda')
-		cy.get('#add_Password').type('1234567')
-        cy.get('#addUsuario').click()
-        cy.get('#closeUsuario').click()
-        
-		
-		
-	})
-})
--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -10,5 +10,18 @@ describe('Probando Input', () => {
    
     
   })
+  it('Test de crear usuario', () => {
+       
+		cy.visit('/iniciosesion')
+		cy.get('#nombre_usuario').type('Giorgioaaa')
+		cy.get('#email_usuario').type('holaaaaaaddosssssssssssssssssooooooosssssooaaaa@gmail.com')
+		cy.get('#address_usuario').type('tegualda')
+		cy.get('#add_Password').type('1234567')
+        cy.get('#addUsuario').click()
+        cy.get('#closeUsuario').click()
+        
+		
+		
+	})
   
 })
